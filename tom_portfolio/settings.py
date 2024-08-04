80f7d935c037a30236ffa452b0456ef9db4020ce
sos-robin <sos.robinson044@gmail.com>
2024-08-05 00:51:25 +0300

    added files

--- tom_portfolio/settings.py
+++ tom_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0h715swx^ih_q2#s2g0*fk35171c2tii3^9x6e8u(sfp#iqrz5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS =  ['.vercel.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
